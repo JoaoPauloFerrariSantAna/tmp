242ce2fda1c15c23a591a1adb830c96fa0f1a8a3
João Paulo Ferrari Sant'Ana <94869217+JoaoPauloFerrariSantAna@users.noreply.github.com>
2026-04-10 19:04:17 -0300

    Create ContaBancaria.cs

---
+++ ContaBancaria.cs
@@ -0,0 +1,9 @@
+namespace ConsoleApp2.Entities
+{
+    public class ContaBancaria
+    {
+        public string Agencia { get; set; }
+        public string Numero { get; set; }
+        public string Banco { get; set; }
+    }
+}
